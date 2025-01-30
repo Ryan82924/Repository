2cc5521432bd33c974f5290cadf0e166621932b8
Ryan82924 <rglenn3@byu.edu>
2025-01-30 12:57:21 -0700

    started javascript for login.jsx. added functionality for logging in. was considering adding authentication but without a database to compare it to it doesn't really work. at the moment the function is almost identical to the html func since there is no auth.

--- simon-html-files/src/Login/Login.jsx
+++ simon-html-files/src/Login/Login.jsx
@@ -1,34 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/header'
 import Footer from '../components/footer';
+import { useNavigate } from 'react-router-dom';
 import '../app.css';
-
-
 export default function Login() {
-
-    return (
-    <div className='fullpage'>
-    <Header />
-    <main className="centered">
-    <h1 className="extrabottommargins extratopmargins size-increase" >Welcome to To-Do Facts</h1>
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const navigate = useNavigate();
+/* java script  functions */
+  function doLogin(event){
     
-    <section>
-        <form method="get" action="To-Do.html">
-        <div>
-          <input className = "bottommargins size-increase" type="text" id="username" name="username" placeholder="Username" /> 
-        </div>
-        <div>
-          <input className ="topmargins size-increase" type="password" placeholder="password (database placeholder)" />
-        </div>
-        <button type="submit" className = "extratopmargins extrabottommargins size-increase">Login</button>
-        <button type="submit" className = "extratopmargins extrabottommargins size-increase">Create</button>
-        </form>
-    </section>
+    event.preventDefault();
+    console.log('logging in')
+    if (username === "validUsername" && password === "validPassword"){
+      navigate("/todo");
+    } 
+    else {
+      alert("Invalid password/user")
+    }
+
       
     
-    
-  </main>
-  <Footer/>
-  </div>
-    )
-}
+  }
+
+
+
+  /* end java script functions */
+
+
+
+
+
+      return (
+      <div className='fullpage'>
+      <Header />
+      <main className="centered">
+      <h1 className="extrabottommargins extratopmargins size-increase" >Welcome to To-Do Facts</h1>
+      
+      <section>
+        <form onSubmit={doLogin}>
+          <div>
+            <input className = "bottommargins size-increase" type="text" id="username" placeholder = "username" name="username" value = {username} onChange={(e) => setUsername(e.target.value)} /> 
+          </div>
+          <div>
+            <input className ="topmargins size-increase" type="password" placeholder="password (database placeholder)" value = {password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit" className = "extratopmargins extrabottommargins size-increase">Login</button>
+          </form>
+      </section>
+      
+      
+    </main>
+    <Footer/>
+    </div>
+      )
+  }
