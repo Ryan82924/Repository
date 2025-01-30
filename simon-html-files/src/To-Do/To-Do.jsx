87a23581a0e4209041d8c8b5584c8f9dc9e36e0c
Ryan82924 <rglenn3@byu.edu>
2025-01-30 13:43:06 -0700

    started on some javascript for the todo page. still trying to make the add task functionality

--- simon-html-files/src/To-Do/To-Do.jsx
+++ simon-html-files/src/To-Do/To-Do.jsx
@@ -1,57 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/header'
 import Footer from '../components/footer';
 import '../app.css';
 
 
 export default function Todo(){ 
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(taskText) {
+    let newTask = {
+      id: Date.now(),
+      text: tasktext,
+      completed: false
+
+    }
+    setTasks(prevTask => [...prevTask, newTask]);
+  function checkTask(id){
+    id
+
+  }
+
+  
+
+  
+/* java script  functions */
+    
+
+
+
+  }
+
+
+
+  /* end java script functions */
+
+
 
     return (
-    <div className='fullpage'>
-    <Header />
-    <main>
-      <section>
-        <h2 className = "task-create"> Create a new task </h2>
-        <form className = "task-create" method = "post" action="/add-task">
-          <label htmlFor ="task-input">Task:</label>
-          <input type="text" id="task-input" name="task" required/>
-          <button type="submit"> Add Task</button>
-        </form>
-      </section>
-      <section>
-        <h2 className = "task-create"> Your Tasks </h2>
-        <section className ="task-create">
-          <div className = "task-item"> 
-            <span> Task 1 (placeholder for tasks pulled from database)</span>
-            <input type="checkbox" id="task1"/>
-          </div>
-          <p className = "less-spacing"> Fun Fact (pulled from third party api) </p>
-          <p id = "third-party-api-placeholder"></p>
-          <div className = "task-item">
-            <span> Task 2 (placeholder for tasks pulled from database)</span>
-            <input type="checkbox" id="task2"/>
-          </div>
-          <p className = "less-spacing"> Fun Fact (pulled from third party api) </p>
-          <p id = "third-party-api-placeholder"></p>
-          <div className = "task-item">        
-            <span> Task 3 (placeholder for tasks pulled from database) </span>
-            <input type="checkbox" id="task3" />
-          </div>
-          <p className = "less-spacing"> Fun Fact (pulled from third party api) </p>
-          <p id = "third-party-api-placeholder"></p>
+      <div className='fullpage'>
+      <Header />
+      <main>
+        <section>
+          <h2 className = "task-create"> Create a new task </h2>
+          <form className = "task-create" method = "post" action="/add-task">
+            <label htmlFor ="task-input">Task:</label>
+            <input type="text" id="task-input" name="task" required/>
+            <button type="submit"> Add Task</button>
+          </form>
         </section>
-       </section>
-
-        
+        <section>
+          <h2 className = "task-create"> Your Tasks </h2>
+          <section className ="task-create">
+            <div className = "task-item"> 
+              <span> Task 1 (placeholder for tasks pulled from database)</span>
+              <input type="checkbox" id="task1" checked = {isChecked} onChange = {handleChange} />
+            </div>
+            <p className = "less-spacing"> Fun Fact (pulled from third party api) </p>
+            <p id = "third-party-api-placeholder"></p>
+            <div className = "task-item">
+              <span> Task 2 (placeholder for tasks pulled from database)</span>
+              <input type="checkbox" id="task2"/>
+            </div>
+            <p className = "less-spacing"> Fun Fact (pulled from third party api) </p>
+            <p id = "third-party-api-placeholder"></p>
+            <div className = "task-item">        
+              <span> Task 3 (placeholder for tasks pulled from database) </span>
+              <input type="checkbox" id="task3" />
+            </div>
+            <p className = "less-spacing"> Fun Fact (pulled from third party api) </p>
+            <p id = "third-party-api-placeholder"></p>
+          </section>
+         </section>
+  
           
-
+            
+  
+          
         
+        
+        
+        </main>
+    <Footer/>
+    </div>
+    )
+  }
       
-      
-      
-      </main>
-  <Footer/>
-  </div>
-  )
+  
+
+
+
 }
     
