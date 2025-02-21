63848e3f3ccb0b323e62183322f2d6460ac0e7be
Ryan82924 <rglenn3@byu.edu>
2025-02-21 13:00:22 -0700

    starting to set up a websocket placeholder using simon as a base guideline

--- simon-html-files/src/Leaderboard/currentleaderboarduser.jsx
+++ simon-html-files/src/Leaderboard/currentleaderboarduser.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../app.css';
 
 
 
+
+
 export function CurrentUser() {
+
+  const [score, setScore] = useState(0);
+  const [username, setUsername] = useState('');
+  useEffect(() => {
+    let intervals = setInterval(() => {
+      (setScore(score))
+      (setUsername(username))}, 1000);
+
+      return () => clearInterval(intervals);
+
+    
+
+    }, [])
+
+  
+
+  
+  function websocketPlaceholder(score, username){
+    
+      
+      
+    }
+    
+    
+    
+  
+    
+  }
+  
+  
   
 
 
@@ -12,9 +44,8 @@ export function CurrentUser() {
     return(
         <div className="leaderboard-entry-current-user">
         <span> Unranked </span> 
-        <span id="rank-3-name"> Current User (placeholder for the Current user's name. updated live via Websocket)</span>
-        <span id="rank-3-score"> 15 (placeholder for the Current user's tasks completed. updated live via Websocket) </span> 
+        <span id="rank-3-name"> {username} (placeholder for the Current user's name. updated live via Websocket)</span>
+        <span id="rank-3-score"> {score} (placeholder for the Current user's tasks completed. updated live via Websocket) </span> 
     
       </div>
     );
-}
