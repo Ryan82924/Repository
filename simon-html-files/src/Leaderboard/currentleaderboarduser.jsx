7ca332e9b6cf38b9539b248775708effb8293bcd
Ryan82924 <rglenn3@byu.edu>
2025-02-21 17:41:24 -0700

    websocket placeholder completed for now, at least for the current user (still need for other positions)

--- simon-html-files/src/Leaderboard/currentleaderboarduser.jsx
+++ simon-html-files/src/Leaderboard/currentleaderboarduser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../app.css';
 
 
@@ -8,30 +8,37 @@ import '../app.css';
 export function CurrentUser() {
 
   const [score, setScore] = useState(0);
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState('Test Username Placeholder');
   useEffect(() => {
+
     let intervals = setInterval(() => {
-      (setScore(score))
-      (setUsername(username))}, 1000);
-
-      return () => clearInterval(intervals);
-
-    
-
-    }, [])
-
-  
-
-  
-  function websocketPlaceholder(score, username){
-    
-      
-      
+    let itemscore = localStorage.getItem('score');
+    if (itemscore){
+      let scores = JSON.parse(itemscore);
+      (setScore(score => scores ))
     }
+
     
+    (setUsername(username))}, 1000);
+
+    return () => clearInterval(intervals);
+
     
-    
+
+    }, [score, username])
+
   
+    
+    
+    
+    return(
+      <div className="leaderboard-entry-current-user">
+      <span> Unranked </span> 
+      <span id="rank-3-name "> {username} ( placeholder for the Current user's name. updated live via Websocket)</span>
+      <span id="rank-3-score "> {score} ( placeholder for the Current user's tasks completed. updated live via Websocket) </span> 
+    
+    </div>
+  );
     
   }
   
@@ -41,11 +48,4 @@ export function CurrentUser() {
 
   
 
-    return(
-        <div className="leaderboard-entry-current-user">
-        <span> Unranked </span> 
-        <span id="rank-3-name"> {username} (placeholder for the Current user's name. updated live via Websocket)</span>
-        <span id="rank-3-score"> {score} (placeholder for the Current user's tasks completed. updated live via Websocket) </span> 
-    
-      </div>
-    );
+  
